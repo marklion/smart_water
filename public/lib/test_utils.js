917821ef95faf5dfd053bac146e26f52b13e060a
marklion <marklion@sina.com>
2025-08-08 17:32:10 +0800

    add some test cases

--- public/lib/test_utils.js
+++ public/lib/test_utils.js
@@ -18,7 +18,7 @@ async function start_server() {
     return ret;
 }
 
-export default async function (processName) {
+export default async function create_cli (processName) {
     let ret = {};
 
     // 解析命令和参数
@@ -40,16 +40,21 @@ export default async function (processName) {
                 //console.log('ret.output:', ret.output);
                 // 找到 output 中包含 prompt 并且 prompt 是行末的行
                 let lines = ret.output.split('\n');
+                lines = lines.map(line => line.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, ''));
                 let promptLine = lines.find(line => {
-                    line = line.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '');
                     return line.endsWith(prompt)
                 });
                 if (promptLine) {
                     lines = lines.slice(1);
+                    lines = lines.filter(line => line.trim() !== '');
                     ret.output = lines.join('\n');
                     clearInterval(checkOutput);
+                    let outputBeforePrompt = ''
                     let index = ret.output.indexOf(promptLine);
-                    let outputBeforePrompt = ret.output.substring(0, index);
+                    if (index > 0 && ret.output[index - 1] === '\n') {
+                        index--;
+                        outputBeforePrompt = ret.output.substring(0, index);
+                    }
                     ret.output = ret.output.substring(index + promptLine.length);
                     resolve(outputBeforePrompt);
                 }
@@ -66,6 +71,21 @@ export default async function (processName) {
         ret.server.kill();
     }
     ret.server = await start_server();
+    ret.save_config = async function() {
+        let new_cli = await create_cli(processName);
+        await new_cli.run_cmd('save');
+        await new_cli.close();
+    }
+    ret.clear_config = async function() {
+        let new_cli = await create_cli(processName);
+        await new_cli.run_cmd('clear');
+        await new_cli.close();
+    }
+    ret.restore_config = async function() {
+        let new_cli = await create_cli(processName);
+        await new_cli.run_cmd('restore');
+        await new_cli.close();
+    }
     await waitForPrompt('sw_cli> ');
 
     return ret;
---
+++ resource/test/farm.test.js
@@ -0,0 +1,55 @@
+import test_utils from "../../public/lib/test_utils";
+let cli;
+beforeAll(async () => {
+    cli = await test_utils('npm run dev_cli');
+    await cli.run_cmd('clear');
+})
+afterAll(async () => {
+    await cli.run_cmd('clear');
+    await cli.run_cmd('save');
+    await cli.close();
+})
+
+describe('配置测试', () => {
+    let farm_configs = [];
+    for (let i = 0; i < 22; i++) {
+        farm_configs.push({
+            name: `farm${i}`,
+            location: `location${i}`,
+            info: (i % 2) ? `info${i}` : undefined
+        })
+    }
+    beforeEach(async () => {
+        await cli.run_cmd('resource')
+        await cli.run_cmd('farm')
+        for (const farm of farm_configs) {
+            await cli.run_cmd(`add farm '${farm.name}' '${farm.location}' ${farm.info ? `'${farm.info}'` : ''}`);
+        }
+    })
+    afterEach(async () => {
+        await cli.run_cmd('clear')
+        await cli.run_cmd('return')
+        await cli.run_cmd('return')
+    })
+
+    test('增删农场', async () => {
+        let bdr = await cli.run_cmd('bdr');
+        expect(bdr.split('\n').length).toBe(22);
+        expect(bdr).toContain('farm13');
+        expect(bdr).not.toContain('info18');
+        await cli.run_cmd('del farm farm10');
+        bdr = await cli.run_cmd('bdr');
+        expect(bdr.split('\n').length).toBe(21);
+        expect(bdr).not.toContain('info10');
+    })
+    test('保存和恢复', async () => {
+        let cur_bdr = await cli.run_cmd('bdr');
+        await cli.save_config();
+        await cli.clear_config();
+        let clear_bdr = await cli.run_cmd('bdr');
+        expect(clear_bdr).toEqual('');
+        await cli.restore_config();
+        let restore_bdr = await cli.run_cmd('bdr');
+        expect(restore_bdr).toEqual(cur_bdr);
+    });
+});
