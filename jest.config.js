b56c1e29956269319fd9e68da8dd846f6c95cadb
marklion <marklion@sina.com>
2025-08-10 10:45:09 +0800

    DPKW-19: Add CI workflow and update configurations (#1)
    * DPKW-19: Add CI workflow and update configurations
    
    * add run
    
    * add init
    
    * make ini ci
    
    * change 4
    
    * ssa
    
    * test again
    
    * test a
    
    * more test
    
    * test more

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,16 @@ export default {
   testEnvironment: 'node',
   transform: {},
   maxWorkers: 1,
+  testTimeout: 30000, // 30秒超时
+  setupFilesAfterEnv: [],
   reporters: [
     "default",
+    "jest-github-actions-reporter",
     ["jest-html-reporters", {
       publicPath: "./test_reports",
       filename: "test-report.html",
       expand: true, // 显示详细测试过程
     }]
-  ]
+  ],
+  testLocationInResults: true
 };
