0be51c80fd135b8113ed32eea863298582fce1f4
marklion <marklion@sina.com>
2025-08-12 13:36:02 +0800

    test again

--- jest.ci.config.js
+++ jest.ci.config.js
@@ -5,10 +5,9 @@ export default {
     testTimeout: 60000, // CI环境中使用更长的超时时间
     forceExit: true, // 强制退出，防止卡住
     detectOpenHandles: true,
-    setupFilesAfterEnv: [],
+    setupFilesAfterEnv: ['jest-allure/dist/setup'],
     reporters: [
-        ['github-actions', { silent: false }],
-        'summary'
+        ['default', 'jest-allure'],
     ],
     testLocationInResults: true,
     // 在CI环境中可能需要的额外配置
