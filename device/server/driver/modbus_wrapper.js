e8ccaf94707a5fa7f379e206d6288380844a4731
marklion <marklion@sina.com>
2025-12-16 09:44:58 +0800

    DPKW-114 add connection pool for modbus TCP

--- device/server/driver/modbus_wrapper.js
+++ device/server/driver/modbus_wrapper.js
@@ -1,12 +1,18 @@
-export default async function () {
-    try {
-        // 尝试 ES Module 导入
-        const mod = await import('modbus-serial');
-        return mod.default || mod;
-    } catch (e) {
-        // 回退到 CommonJS
-        const { createRequire } = await import('module');
-        const require = createRequire(import.meta.url);
-        return require('modbus-serial');
+import ModbusRTU from "modbus-serial";
+class ModbusTcpConnection {
+    constructor() {
+        this.pools = new Map(); // 支持多个连接池
+    }
+    async fetchConnection(ip, port, device_id) {
+        let key = `${ip}:${port}:${device_id}`;
+        if (!this.pools.has(key)) {
+            const client = new ModbusRTU();
+            await client.connectTCP(ip, { port: port });
+            client.setID(device_id);
+            this.pools.set(key, client);
+        }
+        return this.pools.get(key);
     }
 }
+
+export default new ModbusTcpConnection();
